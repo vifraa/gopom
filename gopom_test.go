483284cc4f75c5fc70eeb35661e97e866604955c
Viktor Franzen <viktor_franzen@hotmail.com>
2020-12-13 14:43:06 +0100

    Tested parsing parent fields.

--- gopom_test.go
+++ gopom_test.go
@@ -1,4 +1,28 @@
 package gopom
 
+import (
+	"encoding/xml"
+	"testing"
+)
 
+func TestParsing_Parent(t *testing.T) {
+	var p Project
+	err := xml.Unmarshal([]byte(examplePom), &p)
+	if err != nil {
+		t.Error(err)
+		return
+	}
 
+	if p.Parent.ArtifactID != "test-application" {
+		t.Error("Parent.ArtifactID: expected 'test-application', got: " + p.Parent.ArtifactID)
+	}
+	if p.Parent.GroupID != "com.test" {
+		t.Error("Parent.GroupID: expected 'com.test', got: " + p.Parent.GroupID)
+	}
+	if p.Parent.Version != "1.0.0" {
+		t.Error("Parent.Version: expected '1.0.0', got: " + p.Parent.Version)
+	}
+	if p.Parent.RelativePath != "../pom.xml" {
+		t.Error("Parent.RelativePath: expected '../pom.xml', got: " + p.Parent.RelativePath)
+	}
+}
